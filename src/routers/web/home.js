66686f559c94c1aa696ca4fbc9b6ed9fb076b298
Belen Schmid <belensch@hotmail.com.ar>
2023-02-23 11:41:08 -0300

    desafio clase 30

---
+++ src/routers/web/home.js
@@ -0,0 +1,24 @@
+import { Router } from 'express'
+import { webAuth } from '../../auth/index.js'
+import config from '../../config.js'
+
+import path from 'path'
+
+const productosWebRouter = new Router()
+
+productosWebRouter.get('/home', webAuth, (req, res) => {
+    // res.sendFile('main.html', {root: 'public'})
+    res.render(path.join(process.cwd(), '/views/pages/home.ejs'), { nombre: req.session.nombre })
+})
+
+productosWebRouter.get('/productos-vista-test', (req, res) => {
+    res.sendFile('productos-vista-test.html', {root: 'public'})
+})
+
+productosWebRouter.get('/info', (req, res) => {
+    res.render(path.join(process.cwd(), '/views/pages/info.ejs'), {
+        specs: config.getSpecs()
+    })
+})
+
+export default productosWebRouter
